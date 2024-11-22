c28aec9bc88b8eb3d6343e3eff8f207da59bbc36
Yunonochkaa <156319429+Yunonochkaa@users.noreply.github.com>
2024-11-23 03:10:24 +0500

    Milk

--- src/Milk.java
+++ src/Milk.java
@@ -1,5 +1,5 @@
 public class Milk extends Product implements Food {
-
+    //Single Responsibility Principle - класс Milk описывает только молоко, а не все продукты
     private final String name = "Молоко";
     private final int price = 300;
 
