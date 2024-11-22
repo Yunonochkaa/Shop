88722b6945f15af6c702e6eb8d9049a9769f7460
Yunonochkaa <156319429+Yunonochkaa@users.noreply.github.com>
2024-11-23 03:09:36 +0500

    Banana

--- src/Banana.java
+++ src/Banana.java
@@ -1,5 +1,7 @@
 
 public class Banana extends Product implements Food {
+    //Open Closed Principle - имплементируя интерфейс Food мы добавляем
+    // классу функциональности, не изменяя его код
     private final String name = "Банан";
     private final int price = 200;
 
