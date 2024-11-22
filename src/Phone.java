72158d91fffaa53f10110559ddb6ab7d252df5bf
Yunonochkaa <156319429+Yunonochkaa@users.noreply.github.com>
2024-11-23 03:10:53 +0500

    Phone

--- src/Phone.java
+++ src/Phone.java
@@ -1,5 +1,7 @@
 public class Phone extends Product implements Device, Mobile {
-
+    //Interface Segregation Principle - применяем разные интерфейсы вместо одного большого
+    //Dependency Inversion Principle - разделив интерфейсы мы не зависим от их общей функциональности,
+    // мы можем поменять один интерфейс на другой
     private final String name = "Телефон";
     private final int price = 1000;
 
